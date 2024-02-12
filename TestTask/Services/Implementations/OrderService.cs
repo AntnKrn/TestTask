d80025ca4739dd600ce12407b82a4b93ea378c62
Anton Kuryan <super.tosh2016@yandex.by>
2024-02-13 02:17:40 +0300

    init

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await _context.Orders.OrderByDescending(p => p.Price * p.Quantity).FirstAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.Where(s => s.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<User> GetUser()
+        {
+            return await _context.Users.OrderByDescending(x => x.Orders.Count).FirstAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users.Where(s => s.Status != 0).ToListAsync(); 
+        }
+    }
+}
